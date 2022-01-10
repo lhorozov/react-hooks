da4118445df3a4d32015aacac6b3708b7379367e
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-10 10:22:21 +0200

    useEffect: HTTP requests

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -2,11 +2,9 @@
 // http://localhost:3000/isolated/exercise/05.js
 
 import * as React from 'react'
-// eslint-disable-next-line no-unused-vars
 import VanillaTilt from 'vanilla-tilt'
 
 function Tilt({children}) {
-  // 🐨 create a ref here with React.useRef()
   const tiltRef = React.useRef()
   React.useEffect(() => {
     const tiltNode = tiltRef.current
